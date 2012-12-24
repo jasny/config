72a67df34fa7ddbd2af50ef23a5f049ce5fb089f
Arnold Daniels <arnold@jasny.net>
2012-12-24 13:15:48 +0100

    Trigger warning on json parse error

--- src/Jasny/Config/JsonParser.php
+++ src/Jasny/Config/JsonParser.php
@@ -27,6 +27,37 @@ class JsonParser implements Parser
     public function parse($file)
     {
         $json = file_get_contents($file);
-        return json_decode($json);
+        $data = json_decode($json);
+        
+        if (json_last_error()) trigger_error("Failed to parse json file: " . $this->getJsonError(json_last_error()), E_USER_WARNING);
+        return $data;
+    }
+    
+
+    private function getJsonError($errno)
+    {
+        switch (json_last_error()) {
+            case JSON_ERROR_NONE:
+                return 'No errors';
+            break;
+            case JSON_ERROR_DEPTH:
+                return 'Maximum stack depth exceeded';
+            break;
+            case JSON_ERROR_STATE_MISMATCH:
+                return 'Underflow or the modes mismatch';
+            break;
+            case JSON_ERROR_CTRL_CHAR:
+                return 'Unexpected control character found';
+            break;
+            case JSON_ERROR_SYNTAX:
+                return 'Syntax error, malformed JSON';
+            break;
+            case JSON_ERROR_UTF8:
+                return 'Malformed UTF-8 characters, possibly incorrectly encoded';
+            break;
+            default:
+                return 'Unknown error';
+            break;
+        }        
     }
 }

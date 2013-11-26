66e34231cbf99b08112cc7d11d3924bbfcc292ca
Arnold Daniels <arnold@jasny.net>
2013-11-26 05:39:25 +0100

    Deleted docs

--- docs/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Jasny\\Config"],["c","Jasny\\Config\\DirLoader"],["c","Jasny\\Config\\IniLoader"],["c","Jasny\\Config\\JsonLoader"],["c","Jasny\\Config\\Loader"],["c","Jasny\\Config\\LoadFile"],["c","Jasny\\Config\\MySQLLoader"],["c","Jasny\\Config\\NeonEntity"],["c","Jasny\\Config\\NeonLoader"],["c","Jasny\\Config\\YamlLoader"]];
